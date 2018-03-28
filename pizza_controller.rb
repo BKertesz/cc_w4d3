060daa578e5a6fe3bf535ad79afc0581b1f325b7
BKertesz <kertesz.balazs92@gmail.com>
2018-03-28 16:56:34 +0100

    Working version

---
+++ pizza_controller.rb
@@ -0,0 +1,60 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative("./models/pizza_order")
+
+# index
+get '/pizza_orders' do
+  @orders = PizzaOrder.all
+  erb(:index)
+end
+
+# update
+post '/pizza_orders/:id' do
+  @order = PizzaOrder.new(params)
+
+  @order.update
+  redirect to "/pizza_orders"
+end
+
+# new
+get '/pizza_orders/new' do
+  erb(:new)
+end
+
+
+
+# show
+get '/pizza_orders/:id' do
+  @order = PizzaOrder.find(params[:id].to_i)
+  erb(:show)
+end
+
+
+
+#create
+post '/pizza_orders' do
+  @order = PizzaOrder.new(params)
+  @order.save
+  erb(:create)
+end
+
+# delete
+post '/pizza_orders/:id/delete' do
+  @order = PizzaOrder.find(params[:id].to_i)
+  @order.delete
+  redirect to "/pizza_orders"
+end
+
+# edit
+get '/pizza_orders/:id/edit' do
+  @order = PizzaOrder.find(params[:id].to_i)
+  erb(:edit)
+end
+
+
+
+
+
+
+# EOF
